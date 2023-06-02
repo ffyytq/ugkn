d3c4adaf92149b786b2b755381815894b9639428
ffyytq <135384997+ffyytq@users.noreply.github.com>
2023-06-03 00:21:11 +0300

    Add files via upload

---
+++ script_inde.js
@@ -0,0 +1,250 @@
+
+console.log("None😒")   
+       
+                        
+                    
+                      
+                    function openbu() {
+                        document.getElementById('bu').style.display = "block";
+                    }
+                    //////////////
+                   function ext() {
+    document.getElementById('bu').style.display = "none";                   
+                   }
+                        
+                   
+                   function colerm() {
+      document.getElementById('min').style.display = "none";                 
+                   }     
+                   ///styleweb///
+                   function stylv1() {
+        
+       document.getElementById('bod').style.backgroundImage="url(v5.jpeg)";
+document.getElementById('bod').style.backgroundSize="100% 100%";
+document.getElementById('kk').style.backgroundColor="transparent";
+document.getElementById('k1').style.backgroundColor="transparent";
+document.getElementById('k2').style.backgroundColor="transparent";
+                   }
+                   function stylv2() {
+        
+       document.getElementById('bod').style.backgroundImage="url(v6.jpg)";
+document.getElementById('bod').style.backgroundSize="100% 100%";
+document.getElementById('kk').style.backgroundColor="transparent";
+document.getElementById('k1').style.backgroundColor="transparent";
+document.getElementById('k2').style.backgroundColor="transparent";
+                   }
+                   
+                   function stylv3() {
+        
+       document.getElementById('bod').style.backgroundImage="url(v7.jpg)";
+document.getElementById('bod').style.backgroundSize="100% 100%";
+document.getElementById('kk').style.backgroundColor="transparent";
+document.getElementById('k1').style.backgroundColor="transparent";
+document.getElementById('k2').style.backgroundColor="transparent";
+                   }
+                   function stylv4() {
+        
+       document.getElementById('bod').style.backgroundImage="url(v8.jpg)";
+document.getElementById('bod').style.backgroundSize="100% 100%";
+document.getElementById('s').style.color="#eaffff";
+document.getElementById('kk').style.backgroundColor="rgb(181, 241, 16)";
+document.getElementById('k1').style.backgroundColor="rgb(181, 241, 16)";
+document.getElementById('k2').style.backgroundColor="rgb(181, 241, 16)";
+                   }
+                   function stylv5() {
+        
+       document.getElementById('bod').style.backgroundImage="url(v7.jpg)";
+document.getElementById('bod').style.backgroundSize="100% 100%";
+document.getElementById('kk').style.backgroundColor="transparent";
+document.getElementById('k1').style.backgroundColor="transparent";
+document.getElementById('k2').style.backgroundColor="transparent";
+
+                   }
+           /*       function stylv6() {
+             document.getElementById('bod').style.backgroundAnimation="bod 30s infinite";
+
+                   }*/
+//////icon///
+function bbbb() {
+        
+        document.getElementById('sf').style.transform = "translateX(-26px)" ;}
+        
+function colu() {
+    document.querySelectorAll("li").style.display="none";
+} 
+
+function offb() {
+    pphs.forEach(p => {
+
+  p.style.border ="none";
+})
+}
+function red() {
+    pphs.forEach(p => {
+
+p.style.color ="red";
+  
+})
+}
+  function gold() {
+    pphs.forEach(p => {
+
+p.style.color ="gold";
+  
+})
+}
+function oh() {
+    pphs.forEach(p => {
+
+p.style.color ="#00c0ff";
+  
+})
+}
+function white() {
+    pphs.forEach(p => {
+
+p.style.color ="white";
+  
+})
+}
+/*                   let bod = document.getElementById("bod");
+bod.style.background = co.value ;*/
+function colpp() {
+ 
+let co = document.getElementById("co");
+pphs.forEach(p => {
+ p.style.color = co.value;
+})
+    
+
+}
+
+function colrs() {
+    document.getElementById('minn').style.display = "block";
+}
+function stylv10() {
+        
+       document.getElementById('bod').style.backgroundImage="url(v10.gif)";
+document.getElementById('bod').style.backgroundSize="100% 100%";
+document.getElementById('kk').style.backgroundColor="#ff40bd";
+document.getElementById('k1').style.backgroundColor="#ff40bd";
+document.getElementById('k2').style.backgroundColor="#ff40bd";
+}
+function stylv6() {
+        
+       document.getElementById('bod').style.backgroundImage="url('xx.jpg')";
+document.getElementById('bod').style.backgroundSize="100% 100%";
+document.getElementById('kk').style.backgroundColor="#ff40bd";
+document.getElementById('k1').style.backgroundColor="#ff40bd";
+document.getElementById('k2').style.backgroundColor="#ff40bd";
+}
+  function stylv7() {
+        
+       document.getElementById('bod').style.backgroundImage="url(v9.jpeg)";
+document.getElementById('bod').style.backgroundSize="100% 100%";
+document.getElementById('kk').style.backgroundColor="#ff40bd";
+document.getElementById('k1').style.backgroundColor="#ff40bd";
+document.getElementById('k2').style.backgroundColor="#ff40bd";
+}      
+function stylv8() {
+        
+       document.getElementById('bod').style.backgroundImage="url(v11.jpg)";
+document.getElementById('bod').style.backgroundSize="100% 100%";
+document.getElementById('kk').style.backgroundColor="#ff40bd";
+document.getElementById('k1').style.backgroundColor="#ff40bd";
+document.getElementById('k2').style.backgroundColor="#ff40bd";
+}
+function stylv9() {
+        
+       document.getElementById('bod').style.backgroundImage="url(v13.jpg)";
+document.getElementById('bod').style.backgroundSize="100% 100%";
+document.getElementById('kk').style.backgroundColor="#ff40bd";
+document.getElementById('k1').style.backgroundColor="#ff40bd";
+document.getElementById('k2').style.backgroundColor="#ff40bd";
+}
+function stylv11() {
+        
+       document.getElementById('bod').style.backgroundImage="url(v15.jpg)";
+document.getElementById('bod').style.backgroundSize="100% 100%";
+document.getElementById('kk').style.backgroundColor="transparent";
+document.getElementById('k1').style.backgroundColor="transparent";
+document.getElementById('k2').style.backgroundColor="transparent";
+                   }
+                   function stylv13() {
+        
+       document.getElementById('bod').style.backgroundImage="url(v12.jpg)";
+document.getElementById('bod').style.backgroundSize="100% 100%";
+document.getElementById('kk').style.backgroundColor="transparent";
+document.getElementById('k1').style.backgroundColor="transparent";
+document.getElementById('k2').style.backgroundColor="transparent";
+                   }
+      function oppze() {             
+let xc =document.getElementById("kons");
+let minn =document.getElementById("minn");
+xc.style.display = "block";
+minn.style.display = "none";
+}
+function colst() {
+ let xc =document.getElementById("kons");
+ xc.style.display = "none";
+}
+                   
+////bu///
+/*
+function fyuh() {
+ let actv = document.querySelector("aside");
+ let icc = document.getElementById("icc");
+ let iccc = document.getElementById("iccc");
+ actv.style.transform = "translateX(0%)";
+  icc.style.right = "500px"; 
+  icc.style.display = "none";
+  iccc.style.transform = "translateX(0%)";
+  iccc.style.right = "500px"; 
+ }
+ function cilse() {
+     let actv = document.querySelector("aside");
+ let icc = document.getElementById("icc");
+ let iccc = document.getElementById("iccc");
+ actv.style.transform = "translateX(100%)";
+ iccc.style.transform = "translateX(100%)";
+ icc.style.display = "block";
+ 
+ }
+setInterval(function() {
+  cilse()  
+}10000)
+  function ogfsa() {
+      let ixcvi = 
+
+uu.addEventListener("click", function() {
+   ixcvi.style.color = "yellow";
+   
+   
+})    
+}
+*/
+function cli() {
+    
+}
+
+function loyszv() {
+     const bc = document.getElementById("bace");
+let forv = document.querySelector(".zxsr");
+
+    forv.style.right = "50px"; 
+    
+  } 
+
+/*
+bc.addEventListener("click", function() {
+ forv.classList.toggle("riyu")   
+ 
+})
+*/
+
+let gartdd = document.getElementById("gartdd");
+function srtyu(marty) {
+gartdd.onclick = gartdd.style.backgroundImage = marty;
+
+    
+}
